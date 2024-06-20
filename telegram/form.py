328da6d911ea78d459f2b057422fbc7276d25211
artisaaep <104840798+artisaaep@users.noreply.github.com>
2024-06-20 12:49:05 +0300

    bio removed

--- telegram/form.py
+++ telegram/form.py
@@ -1,5 +1,3 @@
-from random import random
-
 from aiogram import Router
 from aiogram import types
 from bot_init import bot
@@ -17,7 +15,6 @@ class Form(StatesGroup):
     name = State()
     age = State()
     sex = State()
-    bio = State()
     photo = State()
     finish = State()
 
@@ -48,51 +45,42 @@ async def set_age(message: types.Message, state: FSMContext):
             one_time_keyboard=True,
         )
         await message.answer("Укажите ваш пол", reply_markup=markup)
-        await state.set_state(Form.bio)
+        await state.set_state(Form.photo)
     except:
         await message.answer("Введеный возраст некорректен, разрешены только цифры")
         await state.set_state(Form.sex)
 
 
-@router.message(Form.bio)
+@router.message(Form.photo)
 async def set_sex(message: types.Message, state: FSMContext):
-    if message.text == "Мужской":
-        await db.update_profile(user_id=message.from_user.id, sex=0)
-        await message.answer("Пришлите фото, которое будет отображаться в профиле",
-                             reply_markup=ReplyKeyboardRemove())
+    if message.text != "Мужской" and message.text != "Женский":
         await state.set_state(Form.photo)
+    elif message.text == "Мужской":
+        await db.update_profile(user_id=message.from_user.id, sex=0)
     elif message.text == "Женский":
         await db.update_profile(user_id=message.from_user.id, sex=1)
-        await message.answer("Пришлите фото, которое будет отображаться в профиле",
-                             reply_markup=ReplyKeyboardRemove())
-        await state.set_state(Form.photo)
-    else:
-        await state.set_state(Form.bio)
 
-
-@router.message(Form.photo)
-async def set_photo(message: types.Message, state: FSMContext):
-    if message.photo is not None:
-        await bot.download(message.photo[-1], destination=f"photos/{message.from_user.id}.jpg")
-        await message.answer("Ну и наполедок, напишите пару слов о себе, чтобы люди могли получше узнать вас\U0001f643",
-                             reply_markup=ReplyKeyboardRemove())
-        await state.set_state(Form.finish)
-    else:
-        await state.set_state(Form.photo)
+    await message.answer("Пришлите фото, которое будет отображаться в профиле",
+                         reply_markup=ReplyKeyboardRemove())
+    await state.set_state(Form.finish)
 
 
 @router.message(Form.finish)
-async def set_bio(message: types.Message):
-    await db.update_profile(user_id=message.from_user.id, bio=message.text)
+async def set_photo(message: types.Message, state: FSMContext):
+    if message.photo is not None:
+        await bot.download(message.photo[-1], destination=f"photos/{message.from_user.id}.jpg")
+    else:
+        await state.set_state(Form.photo)
     markup = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Создать поездку",
-                            web_app=WebAppInfo(url="http://f0996151.xsph.ru"))], [
+                            web_app=WebAppInfo(url="https://artisaaep.github.io"))], [
                 KeyboardButton(text="Найти поездку",
-                               web_app=WebAppInfo(url="http://f0996151.xsph.ru"))]
+                               web_app=WebAppInfo(url="https://artisaaep.github.io"))]
         ],
         resize_keyboard=True,
         one_time_keyboard=True,
     )
     await message.answer("Отлично!\U0001f973 Анкета создана, можете начать ваше чудесное путешествие\U0001f699",
                          reply_markup=markup)
+
