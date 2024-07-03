3c43b1473cec7aa4276381f7b3765f4b90d3f307
XriXis <i.lobazov@innopolis.university>
2024-07-03 17:26:14 +0300

    Work in general flow

--- web/front/static/scripts/availabletrips.js
+++ web/front/static/scripts/availabletrips.js
@@ -1,4 +1,4 @@
-let url = "https://e8dc-188-130-155-186.ngrok-free.app";
+let url = "https://8aef-188-130-155-165.ngrok-free.app";
 
 async function apply(trip_id) {
     await fetch(url + "/api/trips/" + trip_id + "/rider?riderID=" + window.Telegram.WebApp.initDataUnsafe.user.id, {
@@ -14,27 +14,35 @@ async function apply(trip_id) {
     }).then(response => {
         if (response.ok) {
             const btn = document.getElementById("choose-" + trip_id);
-            btn.onclick = async () => {
-                await fetch(url + "/api/trips/" + trip_id + "/rider?riderID=" + window.Telegram.WebApp.initDataUnsafe.user.id, {
-                    method: "DELETE",
-                }).then(response => {
-                    if (response.ok) {
-                        btn.onclick = decorator(trip_id);
-                        btn.textContent = "Выбрать";
-                    } else {
-                        window.Telegram.WebApp.showAlert("Something went wrong");
-                    }
-                })
-            }
+            btn.onclick = rejectDecorator(trip_id);
             btn.textContent = "Отменить";
         } else {
             window.Telegram.WebApp.showAlert("Something went wrong");
-            console.log(response);
         }
     })
 }
 
-const decorator = (trip_id) => {
+async function reject(trip_id) {
+    await fetch(url + "/api/trips/" + trip_id + "/rider?riderID=" + window.Telegram.WebApp.initDataUnsafe.user.id, {
+        method: "DELETE",
+    }).then(response => {
+        if (response.ok) {
+            const btn = document.getElementById("choose-" + trip_id);
+            btn.onclick = applyDecorator(trip_id);
+            btn.textContent = "Выбрать";
+        } else {
+            window.Telegram.WebApp.showAlert("Something went wrong");
+        }
+    })
+}
+
+const rejectDecorator = (trip_id) => {
+    return () => {
+        return reject(trip_id)
+    }
+}
+
+const applyDecorator = (trip_id) => {
     return () => {
         return apply(trip_id)
     }
@@ -46,25 +54,40 @@ async function main() {
         method: "GET",
     })).json();
     response.forEach(trip => {
+        let is_attached = false;
+        console.log(trip.passengers)
+        for (let index in trip.passengers) {
+            console.log(trip.passengers[index]);
+            if (trip.passengers[index].id !== window.Telegram.WebApp.initDataUnsafe.user.id) {
+                continue;
+            }
+            is_attached = true;
+            break;
+        }
+        const msg = is_attached ? "Отменить" : "Выбрать";
         bar.innerHTML += `
-<div class="card">
-    <img class="avatar" alt="driver-avatar" src="${url}/api/users/${trip.driver.id}/photo">
-    <a class="name">${trip.driver.name}</a>
-    <div class="maininfa">
-        <a class="date">${trip.departure_time}<br></a>
-        <a class="from">${trip.start_location}</a>
-        <a class="strelka">&#8594;</a>
-        <a class="to"><br>${trip.end_location}</a>
-    </div>
-    <div class="pr-ch">
-        <button class="choose" onclick="apply(${trip.id})" id="choose-${trip.id}">Выбрать</button>
-    </div>
-    <div class="dopinfa">
-        <a class="rides">Поездок: ${trip.driver.rides_amount} <br></a>
-        <a class="free-places">Свободных мест: ${trip.available_seats}</a>
-    </div>
-</div>
-`
+            <div class="card">
+                <img class="avatar" alt="driver-avatar" src="${url}/api/users/${trip.driver.id}/photo">
+                <a class="name">${trip.driver.name}</a>
+                <div class="maininfa">
+                    <a class="date">${trip.departure_time}<br></a>
+                    <a class="from">${trip.start_location}</a>
+                    <a class="strelka">&#8594;</a>
+                    <a class="to"><br>${trip.end_location}</a>
+                </div>
+                <div class="pr-ch">
+                    <a class="price">${trip.price} руб.</a>` +
+            (is_attached
+                    ? `<button class="choose" onClick="reject(${trip.id})" id="choose-${trip.id}">Отменить</button>`
+                    : `<button class="choose" onClick="apply(${trip.id})" id="choose-${trip.id}">Выбрать</button>`
+            ) +
+            `</div>
+                <div class="dopinfa">
+                    <a class="rides">Поездок: ${trip.driver.rides_amount} <br></a>
+                    <a class="free-places">Свободных мест: ${trip.available_seats}</a>
+                </div>
+            </div>
+        `
     })
 }
 
