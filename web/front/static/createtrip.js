35d9e208adff5476216b1c5eea1ffa8effd62265
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2024-07-08 00:45:42 +0300

    button to trip created message added

--- web/front/static/createtrip.js
+++ web/front/static/createtrip.js
@@ -156,13 +156,20 @@ function showSummary() {
 async function submitTrip() {
     console.log('Trip submitted:', tripData);
     let id = window.Telegram.WebApp.initDataUnsafe.user.id;
+    let kb = {
+        inline_keyboard: [[{
+            text: 'подробнее',
+            url: 'vk.com'
+        }]]
+    };
     let text = `Ваша поездка *${tripData.origin} - ${tripData.destination}* успешно создана! 🚙
     
 Нажмите на кнопку ниже, чтобы посмотреть подробную информацию о поездке или отредактировать ее ☺️`;
 
     let encodedText = encodeURIComponent(text);
+    let encodedReplyMarkup = encodeURIComponent(JSON.stringify(kb));
 
-    await fetch(`https://api.telegram.org/bot7384436751:AAEZqciLX_e69D26fKjE4i3qzW9J1b-XISc/sendMessage?chat_id=${id}&text=${encodedText}&parse_mode=Markdown`);
+    await fetch(`https://api.telegram.org/bot7384436751:AAEZqciLX_e69D26fKjE4i3qzW9J1b-XISc/sendMessage?chat_id=${id}&text=${encodedText}&parse_mode=Markdown&reply_markup=${encodedReplyMarkup}`);
     // TODO: token from .env
     window.location.href = "tripcreated.html";
 }
