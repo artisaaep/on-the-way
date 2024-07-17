97e391ff14be50bf39c10ae2d6f4535253af651d
artisaaep <104840798+artisaaep@users.noreply.github.com>
2024-07-18 02:21:37 +0300

    jj

--- web/front/OnTheWay/src/routes/profileinfo/+page.svelte
+++ web/front/OnTheWay/src/routes/profileinfo/+page.svelte
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <link rel="stylesheet" href="../OnTheWay/src/routes/profileinfo/profileinfo.css">
+<link rel="stylesheet" href="profileinfo.css">
     <div class="grey-rect">
         <div class="desc-img" id="sub-d">
             <p class="dir-desc" id="submit-data">Редактировать профиль</p>
@@ -39,5 +31,3 @@
                 </tr>
             </table>
     </div>
-</body>
-</html>
