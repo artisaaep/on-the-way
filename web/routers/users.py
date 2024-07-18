597e5246ee2784d2311e5997b2d8be2143cb61dd
artisaaep <104840798+artisaaep@users.noreply.github.com>
2024-07-18 15:59:49 +0300

    jj

--- web/routers/users.py
+++ web/routers/users.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Response, status
 from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 from web.data_models import User
@@ -29,3 +29,17 @@ def read_user_photo(user_id: int, db: Session = Depends(get_db)):
     image_path = Path(__file__).parent.parent.parent / "shared" / "photos" / (str(user_id) + '.jpg')
     return FileResponse(image_path)
 
+
+@router.put("/")
+def update_user(new_user: User, db: Session = Depends(get_db)):
+    user = db.query(UserModel).filter(UserModel.id == new_user.id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User to update is not found")
+    user_attrs = {
+        attr: value for attr, value in vars(new_user).items() if not attr.startswith('_') and attr != 'metadata'
+    }
+    user_attrs['car_ids'] = None if new_user.car_ids is None else ' '.join(map(str, new_user.car_ids))
+    for (key, value) in user_attrs.items():
+        setattr(user, key, value)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
