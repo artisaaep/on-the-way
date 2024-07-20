613b6afbe87259a4658779c5e5992715f884c12b
Maria <mararokkel@mail.ru>
2024-07-20 21:17:13 +0300

    update availableTrips page

--- web/front/OnTheWay/src/lib/TripCard.svelte
+++ web/front/OnTheWay/src/lib/TripCard.svelte
@@ -51,35 +51,42 @@
         }
     }
 </script>
-
-<div class="card">
-    <img class="avatar" alt="driver-avatar" src="{url}/api/users/{trip.driver.id}/photo">
-    <p class="owner_name">{trip.driver.name}</p>
-    <div class="main-info">
-        <div class="from_main">
-            <p class="from">{trip.start_location}</p>
-            <p class="clari-from">{trip.clarify_from}</p>
+<div class="centre">
+    <div class="card">
+        <div class="verh">
+            <img class="avatar" alt="driver-avatar" src="{url}/api/users/{trip.driver.id}/photo">
+            <div class="verhtext">
+                <p class="owner_name">{trip.driver.name}</p>
+                <div class="main-info">
+                    <div class="from_main">
+                        <p class="from">{trip.start_location}</p>
+                        <p class="clari-from">{trip.clarify_from}</p>
+                    </div>
+                    <div class="bott">
+                        <p class="date">{trip.departure_date}</p>
+                        <p class="arrow">&#8594;</p>
+                        <p class="time">{trip.departure_time}</p>
+                    </div>
+                    <div class="to_main">
+                        <p class="to">{trip.end_location}</p>
+                        <p class="clari-to">{trip.clarify_to}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="bott">
-            <p class="date">{trip.departure_date}</p>
-            <p class="arrow">&#8594;</p>
-            <p class="time">{trip.departure_time}</p>
+        <div class="niz">
+            <div class="additional-info">
+                <p class="rides">Поездок: {trip.driver.rides_amount} </p>
+                <p class="free-places">Свободных мест: {trip.available_seats}</p>
+            </div>
+            <div class="pr-ch">
+                <p class="price">{trip.price} руб.</p>
+                {#if isAttached_}
+                    <button class="choose" on:click={() => reject(trip.id)} id="choose-not-ok-{trip.id}">Отменить</button>
+                {:else}
+                    <button class="choose" on:click={() => apply(trip.id)} id="choose-ok-{trip.id}">Выбрать</button>
+                {/if}
+            </div>
         </div>
-        <div class="to_main">
-            <p class="to">{trip.end_location}</p>
-            <p class="clari-to">{trip.clarify_to}</p>
-        </div>
-    </div>
-    <div class="pr-ch">
-        <p class="price">{trip.price} руб.</p>
-        {#if isAttached_}
-            <button class="choose" on:click={() => reject(trip.id)} id="choose-not-ok-{trip.id}">Отменить</button>
-        {:else}
-            <button class="choose" on:click={() => apply(trip.id)} id="choose-ok-{trip.id}">Выбрать</button>
-        {/if}
-    </div>
-    <div class="additional-info">
-        <p class="rides">Поездок: {trip.driver.rides_amount} </p>
-        <p class="free-places">Свободных мест: {trip.available_seats}</p>
     </div>
 </div>
