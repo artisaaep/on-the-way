8378f78849ecd231262755d6fa1cba12d80c318e
AEZuraa <aezuraa@yandex.ru>
2024-07-20 19:54:28 +0300

    updated redact page in profileinfo

--- web/front/OnTheWay/src/routes/profileInfo/+page.svelte
+++ web/front/OnTheWay/src/routes/profileInfo/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import {onMount} from "svelte";
-    import {url} from "../../enviroment.js";
+    import { onMount } from "svelte";
+    import { url } from "../../enviroment.js";
     import './profileInfo.css';
-    import {carFetcher, userFetcher} from "../../lib/fetchers";
-    import type {Car, User} from "$lib/Types";
-    import {draw} from "svelte/transition";
-    import {data} from "../createTrip/Common";
-    import {user} from '../CurrentUser'
+    import { carFetcher, userFetcher } from "../../lib/fetchers";
+    import type { Car, User } from "$lib/Types";
+    import { draw } from "svelte/transition";
+    import { data } from "../createTrip/Common";
+    import { user } from '../CurrentUser'
 
     let cars: Car[] = [];
 
@@ -46,53 +46,55 @@
     }
 
     onMount(main);
-
 </script>
-<div class="grey-rect">
-    <div class="desc-img" id="sub-d">
-        <p class="dir-desc" id="submit-data">Редактировать профиль</p>
+
+<div class="container">
+    <div class="grey-rect">
+        <div class="desc-img" id="sub-d">
+            <p class="dir-desc" id="submit-data">Редактировать профиль</p>
+        </div>
+        <table id="trip">
+            <tr class="line">
+                <td class="param-name">
+                    <p>Имя</p>
+                </td>
+                <td class="param-val">
+                    {#if $user}
+                        <input bind:value={$user.name}>
+                    {/if}
+                </td>
+            </tr>
+            <br>
+            <tr class="line">
+                <td class="param-name">
+                    <p>Возраст</p>
+                </td>
+                <td class="param-val">
+                    {#if $user}
+                        <input bind:value={$user.age}>
+                    {/if}
+                </td>
+            </tr>
+            <br>
+            <tr class="line">
+                <td class="param-name">
+                    <p>Машины</p>
+                </td>
+                <td class="param-val">
+                    <ul>
+                        {#each cars as car}
+                            <li>
+                                <button on:click={()=>removeCar(car.id)}>-</button>
+                                <p>{car.color} {car.brand} {car.number}</p></li>
+                        {/each}
+                    </ul>
+                </td>
+            </tr>
+            <tr class="line">
+                <td class="submit">
+                    <button id="primenit" on:click={submit}>Применить</button>
+                </td>
+            </tr>
+        </table>
     </div>
-    <table id="trip">
-        <tr class="line">
-            <td class="param-name">
-                <p>Имя</p>
-            </td>
-            <td class="param-val">
-                {#if $user}
-                    <input bind:value={$user.name}>
-                {/if}
-            </td>
-        </tr>
-        <br>
-        <tr class="line">
-            <td class="param-name">
-                <p>Возраст</p>
-            </td>
-            <td class="param-val">
-                {#if $user}
-                    <input bind:value={$user.age}>
-                {/if}
-            </td>
-        </tr>
-        <br>
-        <tr class="line">
-            <td class="param-name">
-                <p>Машины</p>
-            </td>
-            <td class="param-val">
-                <ul>
-                    {#each cars as car}
-                        <li>
-                            <button on:click={()=>removeCar(car.id)}>-</button>
-                            <p>{car.color} {car.brand} {car.number}</p></li>
-                    {/each}
-                </ul>
-            </td>
-        </tr>
-        <tr class="line">
-            <td class="submit">
-                <button id="primenit" on:click={submit}>Применить</button>
-            </td>
-        </tr>
-    </table>
 </div>
